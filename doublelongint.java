4a7b8b31ff801bbd25962e9148de252c2bad8495
abishekem26 <74045238+abishekem26@users.noreply.github.com>
2021-09-10 17:04:37 +0530

    Add files via upload

---
+++ doublelongint.java
@@ -0,0 +1,23 @@
+public class ExplicitTypeConversion
+{ 
+	double d = 100.04; 
+	long l;
+	int i;
+	void conversion() 
+	{ 
+    
+	      System.ou.println(“Double value”+d);
+	      
+              l = 100;
+              I = 100;
+              System.ou.println(“Long value”+l);
+             System.out.println(“int value”+i);
+    
+	} 
+	public static void main(String[] args) 
+	{ 
+		ExplicitTypeConversion etc = new ExplicitTypeConversio();
+
+		Etc.conversion(); 
+	} 
+}
